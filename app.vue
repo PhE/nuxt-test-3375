5a172a585abd7ad685df88dea94c027ac032a626
Philippe ENTZMANN <philippe.entzmann@gmail.com>
2024-07-11 09:40:58 +0200

    autre modif avec alcal

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>Here we are on feature CCCCCCCCC branch !!</p>
+    <p>Here we are on feature CCCCCCCCC 2222222 branch !!</p>
     <NuxtRouteAnnouncer />
   </div>
 </template>
